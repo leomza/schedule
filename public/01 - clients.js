43b2b1f7face6ccf171f07b8d4644be85233e7fd
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-09-01 11:01:30 +0300

    up

--- public/01 - clients.js
+++ public/01 - clients.js
@@ -152,21 +152,74 @@ const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 const optionsList = document.querySelectorAll(".option");
 const btn = document.querySelector('.button-form')
-selected.addEventListener("click", () => {
-    optionsContainer.classList.toggle("active");
-  });
+// selected.addEventListener("click", () => {
+//     optionsContainer.classList.toggle("active");
+//   });
  
 
-  optionsList.forEach(o => {
-    o.addEventListener("click", () => {
-      selected.innerHTML = o.querySelector("label").innerHTML;
-      optionsContainer.classList.remove("active");
-    });
-  });
+//   optionsList.forEach(o => {
+//     o.addEventListener("click", () => {
+//       selected.innerHTML = o.querySelector("label").innerHTML;
+//       optionsContainer.classList.remove("active");
+//     });
+//   });
+  const selectedAll = document.querySelectorAll(".selected");
 
+  selectedAll.forEach((selected) => {
+    const optionsContainer = selected.previousElementSibling;
+ 
+  
+    const optionsList = optionsContainer.querySelectorAll(".option");
+  
+    selected.addEventListener("click", () => {
+      if (optionsContainer.classList.contains("active")) {
+        optionsContainer.classList.remove("active");
+      } else {
+        let currentActive = document.querySelector(".options-container.active");
+  
+        if (currentActive) {
+          currentActive.classList.remove("active");
+        }
+  
+        optionsContainer.classList.add("active");
+      }
+  
+       
+    });
+  
+    optionsList.forEach((o) => {
+      o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+      });
+    });
+  
+    
+  
+   
+  });
   selected.addEventListener("click", () => {
     btn.classList.toggle("button-hiden");
   });
+
+
+
+
+//SELECT BOX-TIME
+// const selectedTime = document.querySelector(".selected-time");
+ 
+
+// selectedTime.addEventListener("click", () => {
+//     optionsContainer.classList.toggle("active");
+//   });
+ 
+
+//   optionsList.forEach(o => {
+//     o.addEventListener("click", () => {
+//       selected.innerHTML = o.querySelector("label").innerHTML;
+//       optionsContainer.classList.remove("active");
+//     });
+//   });
 
 //In the "form Edit" I stablish the previous checked value that the element already has 
 function radioButtonCheck(projectType) {
