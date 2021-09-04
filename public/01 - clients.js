aadffb3900446ceaccbf39646c03f7eb7cd5a575
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-09-04 11:40:32 +0300

    up

--- public/01 - clients.js
+++ public/01 - clients.js
@@ -1,44 +1,51 @@
 //Handle the form to create a new Client:
 const handleForm = document.querySelector("#formCreate");
-handleForm.addEventListener('submit', handleNewClient);
+handleForm.addEventListener("submit", handleNewClient);
 
 async function handleNewClient(ev) {
-    try {
-        ev.preventDefault();
-        let { clientname, phone, email, dealTime, callLimitPerDay } = ev.target.elements
-        clientname = clientname.value;
-        phone = phone.value;
-        email = email.value;
-        dealTime = dealTime.value;
-        callLimitPerDay = callLimitPerDay.value;
+  try {
+    ev.preventDefault();
+    let { clientname, phone, email, dealTime, callLimitPerDay } =
+      ev.target.elements;
+    clientname = clientname.value;
+    phone = phone.value;
+    email = email.value;
+    dealTime = dealTime.value;
+    callLimitPerDay = callLimitPerDay.value;
 
-        modalCreate.style.display = "none";
-        ev.target.reset();
+    modalCreate.style.display = "none";
+    ev.target.reset();
 
-        const clientDetails = { clientname, phone, email, dealTime, callLimitPerDay };
-        const clientsCreated = await axios.post('/clients/register', clientDetails);
-        swal("Good job!", "New user added succesfully!", "success");
-        renderClients(clientsCreated.data.allClients.clients);
-    } catch (error) {
-        console.error(error);
-    }
+    const clientDetails = {
+      clientname,
+      phone,
+      email,
+      dealTime,
+      callLimitPerDay,
+    };
+    const clientsCreated = await axios.post("/clients/register", clientDetails);
+    swal("Good job!", "New user added succesfully!", "success");
+    renderClients(clientsCreated.data.allClients.clients);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 //Render all the clients
 async function renderClients(clientsToShow) {
-    try {
-        const table = document.querySelector('.table');
-        if (!table) throw new Error('There is a problem finding table from HTML');
+  try {
+    const table = document.querySelector(".table");
+    if (!table) throw new Error("There is a problem finding table from HTML");
 
-        if (!clientsToShow) {
-            const clientsInfo = await axios.get(`/clients/getAllClients`);
-            const { clients } = clientsInfo.data.allClients;
-            clientsToShow = clients;
-        }
+    if (!clientsToShow) {
+      const clientsInfo = await axios.get(`/clients/getAllClients`);
+      const { clients } = clientsInfo.data.allClients;
+      clientsToShow = clients;
+    }
 
-        let html = clientsToShow.map(element => {
-            return (
-                `<tr>
+    let html = clientsToShow
+      .map((element) => {
+        return `<tr>
             <td>${element.clientname}</td>
             <td>${element.phone}</td> 
             <td>${element.email}</td>
@@ -54,47 +61,78 @@ async function renderClients(clientsToShow) {
           </div>
           </td> 
              
-            </tr>`
-            );
-        }).join('');
+            </tr>`;
+      })
+      .join("");
 
-        table.innerHTML = html;
-
-    } catch (error) {
-        swal("Ohhh no!", error.response.data, "warning");
-        console.error(error);
-    }
+    table.innerHTML = html;
+  } catch (error) {
+    swal("Ohhh no!", error.response.data, "warning");
+    console.error(error);
+  }
 }
+//SELECT BOX
+
+// const optionsContainer = document.querySelector(".options-container");
+// const optionsList = document.querySelectorAll(".option");
+const selectedAll = document.querySelectorAll(".selected");
+
+selectedAll.forEach((selected) => {
+  const optionsContainer = selected.previousElementSibling;
+
+  const optionsList = optionsContainer.querySelectorAll(".option");
+
+  selected.addEventListener("click", () => {
+    if (optionsContainer.classList.contains("active")) {
+      optionsContainer.classList.remove("active");
+    } else {
+      let currentActive = document.querySelector(".options-container.active");
+
+      if (currentActive) {
+        currentActive.classList.remove("active");
+      }
+
+      optionsContainer.classList.add("active");
+    }
+  });
+
+  optionsList.forEach((o) => {
+    o.addEventListener("click", () => {
+      selected.innerHTML = o.querySelector("label").innerHTML;
+      optionsContainer.classList.remove("active");
+      o.children[0].checked = true;
+    });
+  });
+});
 
 //Delete a client
 function removeClient(clientId, clientName) {
-    try {
-        swal({
-            title: "Are you sure?",
-            text: `Once deleted, you will not be able to recover this client ${clientName}!`,
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-        })
-            .then((willDelete) => {
-                if (willDelete) {
-                    deleteClient(clientId);
-                } else {
-                    swal("Your client is safe!");
-                }
-            });
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    swal({
+      title: "Are you sure?",
+      text: `Once deleted, you will not be able to recover this client ${clientName}!`,
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        deleteClient(clientId);
+      } else {
+        swal("Your client is safe!");
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function deleteClient(clientId) {
-    try {
-        const clientsInfo = await axios.delete(`/clients/deleteClient/${clientId}`);
-        renderClients(clientsInfo.data.allClients.clients);
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const clientsInfo = await axios.delete(`/clients/deleteClient/${clientId}`);
+    renderClients(clientsInfo.data.allClients.clients);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 //Update a client:
@@ -102,16 +140,18 @@ async function deleteClient(clientId) {
 let clientIdEdit;
 
 async function editClient(uuidClient) {
-    try {
-        if (!modalEdit) throw new Error('There is a problem finding the modal in the HTML');
-        modalEdit.style.display = "block";
-        modalEdit.classList.add("showModal");
+  try {
+    if (!modalEdit)
+      throw new Error("There is a problem finding the modal in the HTML");
+    modalEdit.style.display = "block";
+    modalEdit.classList.add("showModal");
 
-        const formEdit = document.querySelector("#formEdit");
-        if (!formEdit) throw new Error('There is a problem finding form from HTML');
-        const clientFound = await axios.get(`clients/findClient/${uuidClient}`);
-        const { foundClient } = clientFound.data;
-        let html = `
+    const formEdit = document.querySelector("#formEdit");
+    console.log(formEdit);
+    if (!formEdit) throw new Error("There is a problem finding form from HTML");
+    const clientFound = await axios.get(`clients/findClient/${uuidClient}`);
+    const { foundClient } = clientFound.data;
+    let html = `
                 <div id="checkRadioButton" onmouseenter='radioButtonCheck("${foundClient.dealTime}")'>
                  <h3>Edit client</h3>
 
@@ -128,180 +168,200 @@ async function editClient(uuidClient) {
                     <input type="email" name="email" value="${foundClient.email}" placeholder="Email" required>
                 </div>
 
-                <div>
-                <label for="retainer2">Retainer:</label>
-                <input type="radio" id="retainer2" name="dealTime" value="retainer">
+                <div class="select-box-edit">
+                <div class="options-container-edit">
+                  <div class="option-edit">
+                    <input type="radio" id="retainer2" name="dealTime" value="retainer" />
+                    <label for="retainer">Retainer </label>
+                  </div>
     
-                <label for="hourly2">Hourly:</label>
-                <input type="radio" id="hourly2" name="dealTime" value="hourly">
+                  <div class="option-edit">
+                    <input type="radio" id="hourly2" name="dealTime" value="hourly" />
+                    <label for="hourly">Hourly </label>
+                  </div>
     
-                <label for="project2">Project:</label>
-                <input type="radio" id="project2" name="dealTime" value="project">
-
-                <label for="all2">All:</label>
-                <input type="radio" id="all2" name="dealTime" value="all">
+                  <div class="option-edit">
+                    <input type="radio" id="project2" name="dealTime" value="project" />
+                    <label for="project">Project </label>
+                  </div>
     
+                  <div class="option-edit">
+                    <input type="radio" id="all2" name="dealTime" value="all" />
+                    <label for="all">All </label>
+                  </div>
                 </div>
-                <input type="submit" value="Update client">
-                </div>`
-        formEdit.innerHTML = html;
-        clientIdEdit = foundClient.uuid;
-    } catch (error) {
-        console.error(error);
-    }
-}
-//SELECT BOX
+                <div class="selected-edit">
+                  <div class="selected__img">
+                    <img src="./img/maletin.svg" alt="" />
+                  </div>
+                  <div class="selected__work">
+                    <p>סוג עסקה</p>
+                  </div>
+                </div>
+              </div>
+              </div>
+              <div class="select-box-edit">
+                <div class="options-container-edit">
 
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
-const optionsList = document.querySelectorAll(".option");
-const btn = document.querySelector('.button-form')
-// selected.addEventListener("click", () => {
-//     optionsContainer.classList.toggle("active");
-//   });
+                    <div class="option-edit">
+                    <input type="radio" id="10" name="callLimitPerDay" value="10">
+                    <label for="10">10 minutes per day</label>
+                    </div>
+
+                    <div class="option-edit">
+                    <input type="radio" id="30" name="callLimitPerDay" value="30">
+                    <label for="30">30 minutes per day</label>
+                    </div>
+
+                    <div class="option-edit">
+                    <input type="radio" id="withOutLimit" name="callLimitPerDay" value="withOutLimit">
+                <label for="withOutLimit">With Out Limits:</label>
+                    </div>
+               
+                </div>
+                <div class="selected-edit">
+              <div class="selected__img">
+                <img src="./img/maletin.svg" alt="" />
+              </div>
+              <div class="selected__work">
+                <p>סוג עסקה</p>
+              </div>
+            </div>
+              
+              </div>
+           
 
 
-//   optionsList.forEach(o => {
-//     o.addEventListener("click", () => {
-//       selected.innerHTML = o.querySelector("label").innerHTML;
-//       optionsContainer.classList.remove("active");
-//     });
-//   });
-const selectedAll = document.querySelectorAll(".selected");
 
-selectedAll.forEach((selected) => {
-    const optionsContainer = selected.previousElementSibling;
+              <input type="submit" value="Update client" class="button-form">
+              </div>
+              `;
 
+    formEdit.innerHTML = html;
+    clientIdEdit = foundClient.uuid;
 
-    const optionsList = optionsContainer.querySelectorAll(".option");
+   
+    const selectedAllEdit = document.querySelectorAll(".selected-edit");
 
-    selected.addEventListener("click", () => {
-        if (optionsContainer.classList.contains("active")) {
-            optionsContainer.classList.remove("active");
+    selectedAllEdit.forEach((selected) => {
+      const optionsContainerEdit = selected.previousElementSibling;
+      const optionsListEdit =
+        optionsContainerEdit.querySelectorAll(".option-edit");
+
+      selected.addEventListener("click", () => {
+        if (optionsContainerEdit.classList.contains("active")) {
+          optionsContainerEdit.classList.remove("active");
         } else {
-            let currentActive = document.querySelector(".options-container.active");
+          let currentActive = document.querySelector(
+            ".options-container-edit.active"
+          );
 
-            if (currentActive) {
-                currentActive.classList.remove("active");
-            }
+          if (currentActive) {
+            currentActive.classList.remove("active");
+          }
 
-            optionsContainer.classList.add("active");
+          optionsContainerEdit.classList.add("active");
         }
-
-
-    });
-
-    optionsList.forEach((o) => {
+      });
+      optionsListEdit.forEach((o) => {
         o.addEventListener("click", () => {
-            selected.innerHTML = o.querySelector("label").innerHTML;
-            optionsContainer.classList.remove("active");
-            o.children[0].checked = true
-
+          selected.innerHTML = o.querySelector("label").innerHTML;
+          optionsContainerEdit.classList.remove("active");
+          o.children[0].checked = true;
         });
-
+      });
     });
+    console.log(selectedAllEdit);
+  } catch (error) {
+    console.error(error);
+  }
+}
+ 
 
-    // optionsList.addEventListener('click',()=>{
-
-    // })
-
-    // for (let i = 0; i < optionsList.length; i++) {
-    // if(optionsList[i].type === 'radio' ){
-    //       i.checked
-    // }
-
-    // }
-
-
-
-});
-
-
-
-
-//SELECT BOX-TIME
-// const selectedTime = document.querySelector(".selected-time");
-
-
-// selectedTime.addEventListener("click", () => {
-//     optionsContainer.classList.toggle("active");
-//   });
-
-
-//   optionsList.forEach(o => {
-//     o.addEventListener("click", () => {
-//       selected.innerHTML = o.querySelector("label").innerHTML;
-//       optionsContainer.classList.remove("active");
-//     });
-//   });
-
-//In the "form Edit" I stablish the previous checked value that the element already has 
+//In the "form Edit" I stablish the previous checked value that the element already has
 function radioButtonCheck(dealTime) {
-    try {
-        const elementWithTheEvent = document.querySelector('#checkRadioButton');
-        if (!elementWithTheEvent) throw new Error('The is a problem finding the element to check the radio button');
+  try {
+    const elementWithTheEvent = document.querySelector("#checkRadioButton");
+    if (!elementWithTheEvent)
+      throw new Error(
+        "The is a problem finding the element to check the radio button"
+      );
 
-        const radioRetainer = document.querySelector('#retainer2');
-        if (!radioRetainer) throw new Error('The is a problem finding the element "retainer" radio button');
+    const radioRetainer = document.querySelector("#retainer2");
+    if (!radioRetainer)
+      throw new Error(
+        'The is a problem finding the element "retainer" radio button'
+      );
 
-        const radioHourly = document.querySelector('#hourly2');
-        if (!radioHourly) throw new Error('The is a problem finding the element "hourly" radio button');
+    const radioHourly = document.querySelector("#hourly2");
+    if (!radioHourly)
+      throw new Error(
+        'The is a problem finding the element "hourly" radio button'
+      );
 
-        const radioProject = document.querySelector('#project2');
-        if (!radioProject) throw new Error('The is a problem finding the element "project" radio button');
+    const radioProject = document.querySelector("#project2");
+    if (!radioProject)
+      throw new Error(
+        'The is a problem finding the element "project" radio button'
+      );
 
-        const radioAll = document.querySelector('#all2');
-        if (!radioAll) throw new Error('The is a problem finding the element "all" radio button');
+    const radioAll = document.querySelector("#all2");
+    if (!radioAll)
+      throw new Error(
+        'The is a problem finding the element "all" radio button'
+      );
 
-        switch (dealTime) {
-            case 'retainer':
-                radioRetainer.checked = true;
-                break;
+    switch (dealTime) {
+      case "retainer":
+        radioRetainer.checked = true;
+        break;
 
-            case 'hourly':
-                radioHourly.checked = true;
-                break;
+      case "hourly":
+        radioHourly.checked = true;
+        break;
 
-            case 'project':
-                radioProject.checked = true;
-                break;
+      case "project":
+        radioProject.checked = true;
+        break;
 
-            case 'all':
-                radioAll.checked = true;
-                break;
-        };
+      case "all":
+        radioAll.checked = true;
+        break;
+    }
 
-        //With this the event is going to happen only once
-        elementWithTheEvent.onmouseenter = null;
-    } catch (error) {
-        console.error(error);
-    };
-};
-
-
+    //With this the event is going to happen only once
+    elementWithTheEvent.onmouseenter = null;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 //Handle Edit
 async function handleEdit(ev) {
-    try {
-        let { clientname, phone, email, dealTime } = ev.target.elements;
-        clientname = clientname.value;
-        phone = phone.value;
-        email = email.value;
-        dealTime = dealTime.value;
+  try {
+    let { clientname, phone, email, dealTime } = ev.target.elements;
+    clientname = clientname.value;
+    phone = phone.value;
+    email = email.value;
+    dealTime = dealTime.value;
 
-        if (!clientname || !phone || !email || !dealTime)
-            throw new Error("You need to complete all the fields");
+    if (!clientname || !phone || !email || !dealTime)
+      throw new Error("You need to complete all the fields");
 
-        if (!modalEdit) throw new Error('There is a problem finding modalEdit from HTML');
-        modalEdit.style.display = "none";
-        ev.target.reset();
+    if (!modalEdit)
+      throw new Error("There is a problem finding modalEdit from HTML");
+    modalEdit.style.display = "none";
+    ev.target.reset();
 
-        const clientDetails = { clientname, phone, email, dealTime };
-        const allClients = await axios.put(`/clients/editClient/${clientIdEdit}`, clientDetails);
-        renderClients(allClients);
-    } catch (error) {
-        alert(error)
-        swal("Ohhh no!", `${error}`, "warning");
-        console.error(error);
-    };
-};
+    const clientDetails = { clientname, phone, email, dealTime };
+    const allClients = await axios.put(
+      `/clients/editClient/${clientIdEdit}`,
+      clientDetails
+    );
+    renderClients(allClients);
+  } catch (error) {
+    alert(error);
+    swal("Ohhh no!", `${error}`, "warning");
+    console.error(error);
+  }
+}
