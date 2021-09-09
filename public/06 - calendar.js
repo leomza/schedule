4b5582c8cd1361ba18ef245904993519468c8227
leomza <leo_mza1991@hotmail.com>
2021-09-09 14:55:17 +0300

    Update

--- public/06 - calendar.js
+++ public/06 - calendar.js
@@ -94,24 +94,43 @@ async function getEvents() {
 }
 
 
-var CLIENT_ID = "737686618954-d0k28hcsdajurnrt1mj4v7rhv3p87bd4.apps.googleusercontent.com";
-var API_KEY = "AIzaSyBT-xOdFVT38YxGiOY3fbnblgVlbQ0kfO0";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const CLIENT_ID = "737686618954-d0k28hcsdajurnrt1mj4v7rhv3p87bd4.apps.googleusercontent.com";
+const API_KEY = "AIzaSyBT-xOdFVT38YxGiOY3fbnblgVlbQ0kfO0";
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
+const authorizeButton = document.getElementById('authorize_button');
+const signoutButton = document.getElementById('signout_button');
 
 /**
 *  On load, called to load the auth2 library and API client library.
 */
 function handleClientLoad() {
-gapi.load('client:auth2', initClient);
+    gapi.load('client:auth2', initClient);
 }
 
 /**
@@ -119,22 +138,22 @@ gapi.load('client:auth2', initClient);
 *  listeners.
 */
 function initClient() {
-gapi.client.init({
-apiKey: API_KEY,
-clientId: CLIENT_ID,
-discoveryDocs: DISCOVERY_DOCS,
-scope: SCOPES
-}).then(function () {
-// Listen for sign-in state changes.
-gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-// Handle the initial sign-in state.
-updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-authorizeButton.onclick = handleAuthClick;
-signoutButton.onclick = handleSignoutClick;
-}, function(error) {
-appendPre(JSON.stringify(error, null, 2));
-});
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    }, function (error) {
+        renderCalendarInfo(JSON.stringify(error, null, 2));
+    });
 }
 
 /**
@@ -142,40 +161,28 @@ appendPre(JSON.stringify(error, null, 2));
 *  appropriately. After a sign-in, the API is called.
 */
 function updateSigninStatus(isSignedIn) {
-if (isSignedIn) {
-authorizeButton.style.display = 'none';
-signoutButton.style.display = 'block';
-listUpcomingEvents();
-} else {
-authorizeButton.style.display = 'block';
-signoutButton.style.display = 'none';
-}
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        listUpcomingEvents();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
 }
 
 /**
 *  Sign in the user upon button click.
 */
 function handleAuthClick(event) {
-gapi.auth2.getAuthInstance().signIn();
+    gapi.auth2.getAuthInstance().signIn();
 }
 
 /**
 *  Sign out the user upon button click.
 */
 function handleSignoutClick(event) {
-gapi.auth2.getAuthInstance().signOut();
-}
-
-/**
-* Append a pre element to the body containing the given message
-* as its text node. Used to display the results of the API call.
-*
-* @param {string} message Text to be placed in pre element.
-*/
-function appendPre(message) {
-var pre = document.getElementById('content');
-var textContent = document.createTextNode(message + '\n');
-pre.appendChild(textContent);
+    gapi.auth2.getAuthInstance().signOut();
 }
 
 /**
@@ -183,29 +190,96 @@ pre.appendChild(textContent);
 * the authorized user's calendar. If no events are found an
 * appropriate message is printed.
 */
-function listUpcomingEvents() {
-gapi.client.calendar.events.list({
-'calendarId': 'primary',
-'timeMin': (new Date()).toISOString(),
-'showDeleted': false,
-'singleEvents': true,
-'maxResults': 10,
-'orderBy': 'startTime'
-}).then(function(response) {
-var events = response.result.items;
-appendPre('Upcoming events:');
 
-if (events.length > 0) {
-  for (i = 0; i < events.length; i++) {
-    var event = events[i];
-    var when = event.start.dateTime;
-    if (!when) {
-      when = event.start.date;
+function listUpcomingEvents() {
+    gapi.client.calendar.events.list({
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
+    }).then(function (response) {
+        const events = response.result.items;
+        renderCalendarInfo(events);
+        renderCalendarInfoToday(events);
+    });
+}
+
+function renderCalendarInfo(events) {
+    try {
+        const calendarInfo = document.querySelector('#calendarInfo');
+        let html = '';
+        html = events.map(element => {
+            const calendarStartDate = formatCalendarDate(element.start);
+            return (`
+            <div>
+                <div>
+                    <p>${element.summary}</p>
+                </div>
+
+                <div>
+                    <p>${calendarStartDate}</p>
+                </div>
+            </div>`
+            )
+        }).join('');
+
+        calendarInfo.innerHTML = html;
+    } catch (error) {
+        console.error(error);
     }
-    appendPre(event.summary + ' (' + when + ')')
-  }
-} else {
-  appendPre('No upcoming events found.');
 }
-});
+
+function formatCalendarDate(startDate) {
+    try {
+        if (startDate.dateTime) {
+            startDate.dateTime = moment(startDate.dateTime).format('lll');
+            return startDate.dateTime;
+
+        } else if (startDate.date) {
+            startDate.date = moment(startDate.date).format("MMM Do YY");
+            return startDate.date;
+        } else {
+            startDate = 'No specified'
+            return startDate;
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+/* //Show the events that are going to happen today
+function renderCalendarInfoToday(events) {
+    try {
+        const eventsToday = document.querySelector('#eventsToday');
+        let html = '';
+        html = events.map(element => {
+            const startTime = formatHour(element.start.dateTime);
+            return (`
+            <div>
+                <div>
+                    <p>${element.summary}</p>
+                </div>
+
+                <div>
+                    <p>${startTime}</p>
+                </div>
+            </div>`
+            )
+        }).join('');
+        eventsToday.innerHTML = html;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function formatHour(element){
+    try {
+        let today = moment().format('MMMM Do YYYY');
+        if (moment(element).format('MMMM Do YYYY') === today){
+        }   
+    } catch (error) {
+        console.error(error);
+    }
+} */
