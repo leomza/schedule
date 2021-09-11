2e08d9532c24825553d79a5bcb9121ded8efc38d
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-09-11 23:09:38 +0300

    up

--- public/05 - projectsDate.js
+++ public/05 - projectsDate.js
@@ -20,7 +20,7 @@ async function renderClients() {
                         </div>
                         <div class="projectDate__image">
                                  <div class="projectDate__image__design">
-                                 <img src="./img/Group BBB(2).png" alt="">
+                           <img src="./img/Group 655.png" alt="">
                                     <p>${designDate}</p>
                                 </div>
                 
